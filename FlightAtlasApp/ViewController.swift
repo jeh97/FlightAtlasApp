0488537abc63e5c0ae46a37368ba5972faea319b
Jacob Hausmann <jeh7497@gmail.com>
2020-06-02 11:09:14 -0500

    Initial Commit

---
+++ FlightAtlasApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FlightAtlasApp
+//
+//  Created by Jacob Hausmann on 6/2/20.
+//  Copyright © 2020 Jacob Hausmann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
